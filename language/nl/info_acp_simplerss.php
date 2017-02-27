ffa9299e3a95865526e59a960fbb4d748c7781e2
Ger Bruinsma <gerbruinsma@ktba.com>
2017-02-27 11:14:35 +0100

    Fix language entries

--- language/nl/info_acp_simplerss.php
+++ language/nl/info_acp_simplerss.php
@@ -21,10 +21,10 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 	'ACP_FORUM_ID'					=> 'Feed forum',
 	'ACP_FORUM_ID_EXPLAIN'			=> 'Forum waar nieuwe RSS berichten in geplaatst worden.',
-	'ACP_SETTINGS_EXPLAIN'			=> 'Je kunt RSS feeds toevoegne met onderstaand formulier. Begin met het toevoegen van een RSS URL. Als je feeds toegevoegd hebt, wordt een tabel met deze parameters getoond:',
+	'ACP_SETTINGS_EXPLAIN'			=> 'Je kunt RSS feeds toevoegen met onderstaand formulier. Begin met het toevoegen van een RSS URL. Als je feeds toegevoegd hebt, wordt een tabel met deze parameters getoond:',
 	'ACP_SIMPLERSS_SETTING_SAVED'	=> 'Simple RSS instellingen opgeslagen',
 	'ACP_SIMPLERSS_TITLE'			=> 'Simpele RSS reader',
-	'ADD_FEED'						=> 'URL toevoegen',
+	'ADD_FEED'						=> 'Feed toevoegen',
 	'FETCH_ALL_FEEDS'				=> 'Verwerk alle feeds handmatig',
 	'FEED_URL'						=> 'Feed URL',
 	'FEED_URL_EXPLAIN'				=> 'De URL van de RSS feed, bijv. <code>https://www.phpbb.com/feeds/rss/</code>. Iedere feed URL moet uniek zijn.',
